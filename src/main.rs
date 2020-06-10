de6f62167c6cd21c26514dad84c40ede21f535cf
ju851bah <julian@bahner.de>
2020-06-10 16:46:02 +0200

    Added user input template

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io::{stdin, stdout, Write};
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let mut input = String::new();
+        print!("Tell me what to do: ");
+        let _ = stdout().flush();
+        stdin().read_line(&mut input).expect("Retard!");
+        if let Some('\n') = input.chars().next_back() {
+            input.pop();
+        }
+        if let Some('\r') = input.chars().next_back() {
+            input.pop();
+        }
+        // TODO: here comes parsing of typed in command
+        println!("Input: {}", input);
+    }
 }
