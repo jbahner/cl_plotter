e2d07062f5db1e60527a813b47588de25b530608
Nik Steinbruegge <ni851ste@htwg-konstanz.de>
2020-07-29 18:57:03 +0200

    Move input processing loop into cli interface

--- src/cli_interface/mod.rs
+++ src/cli_interface/mod.rs
@@ -1,4 +1,9 @@
+use std::io::{stdin, stdout, Write};
+
 use terminal_size::{Height, terminal_size, Width};
+
+use super::parser;
+use super::data;
 
 pub struct CliInterface {
     pub x_min: i64,
@@ -9,10 +14,39 @@ pub struct CliInterface {
 }
 
 impl CliInterface {
-
     const UI_LEFT_MARGIN: u16 = 5;
     const UI_HEADER_SPACE: u16 = 10;
     const UI_FOOTER_SPACE: u16 = 5;
+
+    pub fn cli_interface_loop() {
+        let mut p = parser::Parser::new();
+
+        loop {
+            let mut input = String::new();
+            print!("Type in an expression: ");
+            let _ = stdout().flush();
+            stdin().read_line(&mut input).expect("Retard!");
+            if let Some('\n') = input.chars().next_back() {
+                input.pop();
+            }
+            if let Some('\r') = input.chars().next_back() {
+                input.pop();
+            }
+            // TODO: Validate input format: only one variable allowed
+            // TODO: here comes parsing of typed in command
+            if input == String::from("exit") {
+                println!("Bye, have a beautiful time!");
+                std::process::exit(0);
+            }
+            p.parse_expression(input);
+            p.display_expression();
+
+            // Fill data
+            data::Data::new(p.clone().stack.first().unwrap()).evaluate();
+
+            p.clear();
+        }
+    }
 
 
     pub fn render_functions() {
@@ -20,6 +54,10 @@ impl CliInterface {
 
         // let first_function = functions.first().expect("Failed");
         Self::draw_ui()
+    }
+
+    fn draw_start_screen() {
+
     }
 
     fn draw_ui() {
--- src/main.rs
+++ src/main.rs
@@ -6,36 +6,14 @@ mod cli_interface;
 
 fn main() {
     let mut p = parser::Parser::new();
-    loop {
-        let mut input = String::new();
-        print!("Type in an expression: ");
-        let _ = stdout().flush();
-        stdin().read_line(&mut input).expect("Retard!");
-        if let Some('\n') = input.chars().next_back() {
-            input.pop();
-        }
-        if let Some('\r') = input.chars().next_back() {
-            input.pop();
-        }
-        // TODO: Validate input format: only one variable allowed
-        // TODO: here comes parsing of typed in command
-        if input == String::from("exit") {
-            println!("Bye, have a beautiful time!");
-            std::process::exit(0);
-        }
-        p.parse_expression(input);
-        p.display_expression();
 
-        // Fill data
-        data::Data::new(p.clone().stack.first().unwrap()).evaluate();
 
-        p.clear();
 
 
         // print!("Tell me what to do: ");
         // let _ = stdout().flush();
         // stdin().read_line(&mut input).expect("Retard!");
-        // cli_interface::CliInterface::render_functions()
+        cli_interface::CliInterface::cli_interface_loop()
         //
         // if let Some('\n') = input.chars().next_back() {
         //     input.pop();
@@ -45,5 +23,4 @@ fn main() {
         // }
         // TODO: here comes parsing of typed in command
         // println!("Input: {}", input);
-    }
 }
